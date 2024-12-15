ee82df213be63c9723fb4031bf861db0a6dd1fa8
logaxpapp <logaxpapp@gmail.com>
2024-12-15 03:22:49 -0600

    more Updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
+  preview: {
+    port: 5000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Optional: Simplify imports
+    },
+  },
+  esbuild: {
+    
+  },
 })
